60d83106655ed431e028233af595f270e068b9f6
Akzaoixyo <shangtong7767@126.com>
2021-06-08 12:53:07 +0800

    简单创建我的页面顶部View布局

--- 自做商城项目/我的/controller/AllenMyViewController.m
+++ 自做商城项目/我的/controller/AllenMyViewController.m
@@ -7,17 +7,92 @@
 //
 
 #import "AllenMyViewController.h"
-
+#import <Masonry.h>
+/*--
+ 顶部登陆与注册
+ --AllenL*/
 @interface AllenMyViewController ()
-
+@property (strong , nonatomic) UIView *headView;
+@property (strong , nonatomic) UIImageView *headBackImage;
+@property (strong ,nonatomic) UIButton *loginBtn;
+@property (strong ,nonatomic) UIButton *landingBtn;
 @end
 
 @implementation AllenMyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.edgesForExtendedLayout = 0;
     self.view.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:self.headView];
+    [self.headView addSubview:self.headBackImage];
+    [self.headView addSubview:self.loginBtn];
+    [self.headView addSubview: self.landingBtn];
+    
+    
+    __weak typeof (self) weakSelf = self;
+    [_headView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.equalTo(weakSelf.view);
+        make.height.mas_equalTo(150);
+    }];
+
+    [_headBackImage mas_makeConstraints:^(MASConstraintMaker *make){
+        make.edges.equalTo(weakSelf.headView);
+    }];
+    
+    [_loginBtn mas_makeConstraints:^(MASConstraintMaker *make){
+        make.centerX.equalTo(weakSelf.headView.mas_centerX).offset(60);
+        make.centerY.equalTo(weakSelf.headView.mas_centerY);
+        make.size.mas_equalTo(CGSizeMake(45, 23));
+    }];
+    [_landingBtn mas_makeConstraints:^(MASConstraintMaker *make){
+        make.centerX.equalTo(weakSelf.headView.mas_centerX).offset(-60);
+        make.centerY.equalTo(weakSelf.headView.mas_centerY);
+        make.size.mas_equalTo(CGSizeMake(45, 23));
+    }];
+    
     // Do any additional setup after loading the view.
+}
+
+
+
+- (UIView *)headView{
+    if (!_headView) {
+        _headView = [[UIView alloc]init];
+    }
+    return _headView;
+}
+
+- (UIImageView *)headBackImage{
+    if (!_headBackImage) {
+        _headBackImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"我的背景"]];
+    }
+    return _headBackImage;
+
+}
+
+- (UIButton *)loginBtn{
+    if (!_loginBtn) {
+        _loginBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_loginBtn setTitle:@"登陆" forState:(UIControlStateNormal)];
+        _loginBtn.titleLabel.font = [UIFont systemFontOfSize:18.0f];
+        _loginBtn.titleLabel.textColor = [UIColor whiteColor];
+    }
+    return _loginBtn;
+
+}
+
+
+- (UIButton *)landingBtn{
+    if (!_landingBtn) {
+        _landingBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_landingBtn setTitle:@"注册" forState:(UIControlStateNormal)];
+        _landingBtn.titleLabel.font = [UIFont systemFontOfSize:18.0f];
+        _landingBtn.titleLabel.textColor = [UIColor whiteColor];
+    }
+    return _landingBtn;
+    
 }
 
 - (void)didReceiveMemoryWarning {
